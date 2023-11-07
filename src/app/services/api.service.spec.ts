07ba720698913a212601dfbfe111bf1966ba46c0
Jayash Raj Singh Yadav <jayash.yadav@geminisolutions.com>
2023-11-07 16:04:10 +0530

    unit test cases for api service and app component

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get user data from API', () => {
+    const username = 'testuser';
+    const mockUserData = { login: 'testuser', name: 'Test User' };
+
+    service.getUser(username).subscribe(user => {
+      expect(user).toEqual(mockUserData);
+    });
+
+    const req = httpMock.expectOne(`https://api.github.com/users/${username}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUserData);
+  });
+
+  it('should get repositories data from API', () => {
+    const username = 'testuser';
+    const page = 1;
+    const perPage = 10;
+    const mockRepositoriesData = [{ name: 'repo1' }, { name: 'repo2' }];
+
+    service.getRepositories(username, page, perPage).subscribe(repos => {
+      expect(repos).toEqual(mockRepositoriesData);
+    });
+
+    const req = httpMock.expectOne(`https://api.github.com/users/${username}/repos?page=${page}&per_page=${perPage}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockRepositoriesData);
   });
 });
